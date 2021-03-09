20647b2c2ff034e956838fddec93473c552ed012
Kristian <mkristian@live.ru>
2021-03-09 23:37:11 +0400

    Added component

---
+++ src/components/excel/Excel.js
@@ -0,0 +1,11 @@
+export class Excel {
+    constructor(selector, options) {
+        this.$el = document.querySelector(selector);
+        this.components = options.components || [];
+    }
+
+    render() {
+        console.log(this.$el);
+        this.$el.innerHTML = 'Kris'
+    } 
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import './scss/index.scss'
+import {Excel} from './components/excel/excel';
+import {Formula} from './components/formula/formula';
+import {Header} from './components/header/header';
+import {Table} from './components/table/table';
+import {Toolbar} from './components/toolbar/toolbar';
+import './scss/index.scss';
 
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table],
+});
+ 
+excel.render(); 
 
-const test = async () => {
-   return await Promise.resolve('Working18'); 
-}
-
-test().then((data)=> console.log(data));
+     
